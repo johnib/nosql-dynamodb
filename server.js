0a3968413d8db5aa46b0cf8600e7b6466e8686cd
Jonathan Yaniv <johnitravolta@gmail.com>
2016-06-11 15:25:00 +0300

    updated deps. website boilerplate.

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
-/**
- * Created by johni on 11/06/2016.
- */
+"use strict";
+
+let PORT = process.env.PORT || 3000;
+
+//noinspection JSUnusedLocalSymbols
+let express = require('express'),
+  morgan = require('morgan'),
+  redis = require('ioredis'),
+  aws = require('aws-sdk');
+
+aws.config.update({region: 'eu-central-1'});
+
+let app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/www'));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
